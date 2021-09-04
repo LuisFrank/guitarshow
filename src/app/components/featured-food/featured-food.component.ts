2ed6e07c3c70a8e4f785993a9b5708948932e692
Luis Frank Florentino Medrano <lflorentinomedrano@gmail.com>
2021-09-04 00:57:16 -0500

    first version fret and tuner

--- src/app/components/featured-food/featured-food.component.ts
+++ src/app/components/featured-food/featured-food.component.ts
@@ -1,16 +1,190 @@
+import { ElementRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { featured } from 'src/app/mocks/index';
+import { featured } from 'src/app/mocks/index'
+
+
+
+declare var ml5: any;
+declare var p5: any;
+// declare var stream: any;
+let pitch :any;
+
+
+// declare var setup: any;
+
 @Component({
   selector: 'app-featured-food',
   templateUrl: './featured-food.component.html',
   styleUrls: ['./featured-food.component.scss']
 })
-export class FeaturedFoodComponent implements OnInit {
+export class FeaturedFoodComponent implements OnInit  {
 
   featured: any = featured;
-  constructor() { }
+  start:boolean = false;
+  // mic: any;
 
-  ngOnInit(): void {
+  // audioInput = null;
+  // microphone_stream = null;
+  // gain_node = null;
+  // script_processor_node = null;
+  // script_processor_fft_node = null;
+  // analyserNode = null;
+
+  // play: boolean = false;
+  
+  audioContext:any;
+  mic :any;
+  pitch :any;
+  stream: any;
+  frequency:any = 0;
+  threshold = 1;
+  notes = [
+    // E: 82 Hz (E2 Musical Note)
+
+    // A: 110 Hz (A2 Musical Note)
+
+    // D: 147 Hz (D3 Musical Note)
+
+    // G: 196 Hz (G3 Musical Note)
+
+    // B: 247 Hz (B3 Musical Note)
+
+    // E: 330 Hz (E4 Musical Note)
+
+    {note:'E',freq: 82.407},
+    {note:'A',freq: 110},
+    {note:'D',freq: 146.832},
+    {note:'G',freq: 195.998},
+    {note:'B',freq: 246.942},
+    {note:'E',freq: 329.628}
+
+  ];
+
+  constructor(private elementRef:ElementRef) {
+   
+   }
+ 
+
+   Iniciar(){
+    this.demo();
+   
+   }
+
+  async demo(){
+    this.audioContext = new AudioContext();
+    var stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    this.startPitch(stream,this.audioContext);   
   }
 
+  startPitch(stream:any,context:any){
+    var pitch = ml5.pitchDetection('././assets/crepe_models_3',
+    context , 
+    stream,
+    () =>{
+      console.log("model loaded", ":)");  
+      
+        this.getPitch(pitch);
+    }    
+    )  
+  }
+
+
+  getPitch(pitch:any) {  
+
+      pitch.getPitch((err:any, frequency:any) => {
+        if (frequency) {
+            console.log("frequency",frequency);
+            this.frequency=frequency;
+        } else {
+            console.log("err",err);
+        }
+        this.getPitch(pitch);
+      })
+  } 
+
+
+
+  ngOnInit(): void {  
+    this.loadCanvas();
+  }
+
+  loadCanvas(){
+    const s = (p:any) => {
+
+      let canvas;        
+
+      p.preload = () => {
+        console.log('preload');        
+      }               
+
+      p.setup = () => {
+        canvas = p.createCanvas(400, 400);        
+        canvas.parent('draws');       
+      }
+
+      p.draw = () => {
+       
+        p.background(0);       
+        p.textAlign(p.CENTER,p.CENTER);  
+        p.fill(255);
+        p.textSize(32);      
+        p.text(this.frequency.toFixed(2),p.width /2,  p.height-150);
+       
+
+
+        var closestNote:any;
+        let recordDiff = Infinity;
+
+        for (let index = 0; index < this.notes.length; index++) {
+          let diff = this.frequency - this.notes[index].freq;
+          if(p.abs(diff)<p.abs(recordDiff)){
+            closestNote = this.notes[index];
+            recordDiff = diff;
+          }        
+        }
+
+        p.textSize(64);
+        p.text(closestNote.note,p.width /2,  p.height-50);
+
+
+        let diff = recordDiff;
+
+        let alpha = p.map(p.abs(diff),0,100,255,0);
+        p.rectMode(p.CENTER);
+        p.fill(255,alpha);
+        p.stroke(255);
+        p.strokeWeight(1);
+        if(p.abs(diff) < this.threshold ){
+          p.fill(0,255,0);
+        }        
+        p.rect(200,100,200,50);
+
+        p.stroke(255);
+        p.strokeWeight(4);
+        p.line(200,0,200,200);
+
+        p.noStroke();
+        p.fill(255,0,0);
+        if(p.abs(diff)< this.threshold){
+          p.fill(0,255,0);
+        }
+
+        p.rect(200+ diff,100,10,75);
+
+
+      };
+      
+    }
+    let player = new p5(s);    
+
+  }
+  
+
+
+  
+
+  
 }
+
+
+
--- src/app/components/social-feed/social-feed.component.ts
+++ src/app/components/social-feed/social-feed.component.ts
@@ -1,5 +1,9 @@
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
+import { ChangeDetectorRef } from '@angular/core';
+import { Renderer2, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-
+import {  Scale, Distance, Note } from "tonal";
 @Component({
   selector: 'app-social-feed',
   templateUrl: './social-feed.component.html',
@@ -7,9 +11,253 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialFeedComponent implements OnInit {
 
-  constructor() { }
+  root = document.documentElement;
+  numbersOfFrets:any = 20;
+  numberOfString:any = 6; 
+  singleFretMarkPositions = [3, 5, 7, 9, 15, 19, 21 ];
+  doubleFretMarkPositions = [12,24];
+  opacity:any = 0;
+  shouldShow = true;
+
+  currentScaleName:any;
+  currentNote:any;
+
+  notesFlat = ["C","Db","D","Eb","E","F","Gb","G","Ab","A","Bb","B"];
+  notesSharp = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
+
+  accidentals = 'flats';
+  accidentals_sharps = 'sharps';
+
+  guitarTuning = [4,11,7,2,9,4];
+
+  allNotes:any;
+  showMultipleNotes = false;
+  
+
+  @ViewChild('fretboard',{static:true}) fretboard:any;
+
+  
+  constructor(private cdr:ChangeDetectorRef,private renderer: Renderer2,) {
+    setTimeout(() => {
+     
+      this.cdr.detectChanges();
+    });  
+  
+   }
 
   ngOnInit(): void {
+    this.renderer.setProperty(this.root,'--notepacity',this.opacity);
+    this.renderer.setProperty(this.root,'--number-of-strings',this.numberOfString);
+    // this.root.style.setProperty('--number-of-strings',this.numberOfString);
+    this.setupFretBoard();
+    // this.setupEventListeners();
   }
 
+
+
+  setupFretBoard(){   
+      // this.renderer.setProperty(this.root,'note-dot-opacity',this.opacity);
+      // this.root.style.setProperty('note-dot-opacity',this.opacity);
+    
+      // this.renderer.setStyle(this.root,'note-dot-opacity',this.opacity);
+    //Add strings to fretboard
+      for (let index = 0; index < this.numberOfString; index++) {
+        var child = this.renderer.createElement('div');
+        child.classList.add('string');
+        this.renderer.appendChild(this.fretboard.nativeElement, child);
+      
+
+
+        //create frets
+        for (let fret = 0; fret < this.numbersOfFrets; fret++) {
+          var noteFret = this.renderer.createElement('div');       
+          noteFret.classList.add('note-fret');
+          noteFret.classList.add('note-hide');
+
+          var noteName = this.generateNoteNames((fret+this.guitarTuning[index]),this.accidentals);
+          noteFret.setAttribute('data-note',noteName);
+          noteFret.setAttribute('data-chroma',Note.chroma(noteName));
+          noteFret.setAttribute('data-note-index',fret);
+
+          //Add single fret marks
+          if(index === 0 && this.singleFretMarkPositions.indexOf(fret) !== -1){
+             noteFret.classList.add('single-fretmark');
+          }
+
+           //Add double fret marks
+          if(index === 0 && this.doubleFretMarkPositions.indexOf(fret) !== -1){
+            var doubleFretMark = this.renderer.createElement('div');   
+            doubleFretMark.classList.add('double-fretmark');
+            noteFret.appendChild(doubleFretMark);
+          }
+
+          child.appendChild(noteFret);
+
+          
+        }
+      }
+  
+  }
+
+  generateNoteNames(noteIndex:any,accidentals:any) : string{
+    noteIndex = noteIndex % 12;
+    let noteName = "";
+
+    if(accidentals === 'flats'){
+        noteName= this.notesFlat[noteIndex];
+
+    }else if (accidentals === 'sharps'){
+      noteName= this.notesSharp[noteIndex];
+
+    }
+    return noteName;
+
+  }
+
+  setupEventListeners(){
+
+    this.renderer.listen(this.fretboard.nativeElement, 'mouseover', (evt) => {
+      if(evt.target.classList.contains('note-fret')){      
+         
+          if(this.showMultipleNotes){
+            this.toggleMultipleNotes( evt.target.dataset.note,1);
+          }else{
+            evt.target.classList.remove("note-hide");
+            evt.target.classList.add("note-show");
+  
+
+          }
+      }
+    });
+
+    this.renderer.listen(this.fretboard.nativeElement, 'mouseout', (evt) => {
+      if(evt.target.classList.contains('note-fret')){
+      
+
+        if(this.showMultipleNotes ){
+          this.toggleMultipleNotes( evt.target.dataset.note,0);
+        }else{
+          evt.target.classList.remove("note-show");
+          evt.target.classList.add("note-hide");
+        }
+      }     
+    });
+
+  }
+
+
+  showAllNotes(event: any){
+    if(event.target.checked){
+      this.showMultipleNotes = !this.showMultipleNotes;
+      console.log("ss",event.target.checked);
+    }  else{
+      this.showMultipleNotes = !this.showMultipleNotes;
+    } 
+ }
+
+  toggleMultipleNotes(noteName:any,opacity:any){
+    this.allNotes = document.querySelectorAll('.note-fret');
+    for (let index = 0; index < this.allNotes.length; index++) {
+        if(this.allNotes[index].dataset.note === noteName){
+          if(opacity ==1){
+            // this.allNotes[index].style.setProperty('noteDotOpacity',opacity)
+            this.allNotes[index].classList.remove("note-hide");
+            this.allNotes[index].classList.add("note-show");          
+          }else{
+            // this.allNotes[index].style.setProperty('noteDotOpacity',opacity)
+            this.allNotes[index].classList.remove("note-show");
+            this.allNotes[index].classList.add("note-hide");
+          }
+          
+        }
+     
+      
+    }
+ }
+
+ setNote(noteName:any){
+    this.currentNote = noteName;
+    this.getScales();
+ }
+
+ getScalesName(scaleName:any){
+       
+  this.currentScaleName = scaleName;      
+      this.getScales();
+
+ }
+
+ getScales(){
+  
+  // console.log("Scale names.", Scale.names(false));
+  // console.log("currentNote.",this.currentNote);
+  // console.log("currentScaleName.",this.currentScaleName);
+  // console.log("Scale names.",this.currentNote + " "+ this.currentScaleName);
+    if(this.currentNote != undefined && this.currentScaleName != undefined){
+    
+      console.log("Scale notes and name.",this.currentNote + " "+ this.currentScaleName);
+      console.log("Scale notes.", Scale.notes(this.currentNote + " "+ this.currentScaleName));
+      this.allNotes = document.querySelectorAll('.note-fret');
+
+      // for (var element of this.allNotes) {
+
+      for (let index = 0; index < this.allNotes.length; index++) {
+
+        if( this.allNotes[index].classList.contains('note-show')){
+          this.allNotes[index].classList.remove("note-show");
+          this.allNotes[index].classList.add("note-hide");   
+        }else if( this.allNotes[index].classList.contains('note-show-root')){
+          this.allNotes[index].classList.remove("note-show-root");
+          this.allNotes[index].classList.add("note-hide");
+        }
+      }
+    
+
+      for (let index = 0; index < this.allNotes.length; index++) {
+
+
+       
+        console.log("scale", Scale.notes(this.currentNote + " "+ this.currentScaleName));
+        Scale.notes(this.currentNote + " "+ this.currentScaleName).forEach((noteName, secondIndex) => {
+          console.log("Note.chroma",Note.chroma(noteName));
+          console.log("dataset.chroma",this.allNotes[index].dataset.chroma);
+          if(Number(this.allNotes[index].dataset.chroma) === Note.chroma(noteName)){
+            if(secondIndex == 0){
+              this.allNotes[index].classList.add("note-show-root"); 
+              this.allNotes[index].classList.remove("note-hide");
+            }else{
+              this.allNotes[index].classList.add("note-show");   
+              this.allNotes[index].classList.remove("note-hide");  
+            }
+
+          }
+
+            // if(this.allNotes[index].dataset.note === noteName){
+
+            //     if(Note.chroma(noteName) == 0){
+
+            //     }
+
+
+            //     if(secondIndex==0){
+            //       //root
+            //       this.allNotes[index].classList.add("note-show-root"); 
+            //       this.allNotes[index].classList.remove("note-hide");
+            //     }else{
+            //       this.allNotes[index].classList.add("note-show");   
+            //       this.allNotes[index].classList.remove("note-hide");   
+            //     }
+              
+            // }
+
+       });
+
+      }
+    }
+   
+
+ }
+
+
+ 
 }
